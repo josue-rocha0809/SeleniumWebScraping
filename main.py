1d61a4469aaa9ca6ad2ba909171f98fae3a092ac
Josue Rocha <josue.rocha0809@gmail.com>
2023-11-30 18:20:13 -0600

    dev: getting data from page

--- main.py
+++ main.py
@@ -1,88 +1,63 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
 import time
-import pandas as pd
 
 options = webdriver.ChromeOptions()
 options.add_argument('--start-maximized')
 options.add_argument('--disabled-extensions')
+options.add_argument("--headless")
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--disable-gpu")
+completed = False
 
-driver_path = "C:/Cursos/bigData/selenium/chrome/chromedriver.exe"
-driver = webdriver.Chrome(driver_path, chrome_options=options)
-
-# driver.get('https://eltiempo.es')
+driver_path = "C:/Users/josue/Projects/python/chrome/chromedriver.exe"
+driver: WebDriver = webdriver.Chrome(driver_path, chrome_options=options)
 driver.get('https://www.canada.ca/en/immigration-refugees-citizenship/services/application/check-processing-times.html')
 
-# WebDriverWait(driver, 5) \
-#     .until(EC.element_to_be_clickable((By.CLASS_NAME,
-#                                        'mfp-close'.replace(
-#                                            ' ', '.')))) \
-#     .click()
 try:
-    WebDriverWait(driver, 5).until(
+    WebDriverWait(driver, 2).until(
         EC.element_to_be_clickable((By.CLASS_NAME, 'mfp-close'))
     ).click()
 except TimeoutException:
     print("El elemento 'mfp-close' no está presente o no es clickeable.")
 
-WebDriverWait(driver, 5) \
-    .until(EC.element_to_be_clickable((By.NAME,
-                                       'wb-fieldflowwb-auto-1wb-auto-23'.replace(
+select_element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//div[@id='wb-auto-6']//select[1]"))
+)
+select_obj = Select(select_element)
+select_obj.select_by_visible_text("Temporary residence (visiting, studying, working)")
+driver.save_screenshot("screenshot.png")
+
+select_element2 = WebDriverWait(driver, 20).until(
+     EC.element_to_be_clickable((By.XPATH, "//div[@id='wb-auto-6']//select[2]"))
+)
+
+select_obj2 = Select(select_element2)
+select_obj2.select_by_visible_text("Work permit (from outside Canada)")
+
+select_element3 = WebDriverWait(driver, 300).until(
+    EC.element_to_be_clickable((By.XPATH, "//div[@id='wb-auto-6']//select[3]"))
+)
+select_obj3 = Select(select_element3)
+select_obj3.select_by_visible_text("Mexico")
+completed = True
+if completed:
+    WebDriverWait(driver, 10) \
+       .until(EC.element_to_be_clickable((By.CLASS_NAME,
+                                          'btn btn-primary mrgn-bttm-md'.replace(
                                            ' ', '.')))) \
-    .click()
-WebDriverWait(driver, 5) \
-    .until(EC.element_to_be_clickable((By.NAME,
-                                       'wb-fieldflowwb-auto-1wb-auto-23'))) \
-    .send_keys('Temporary residence (visiting, studying, working)')
-WebDriverWait(driver, 5) \
-    .until(EC.element_to_be_clickable((By.CLASS_NAME,
-                                       'pagedetails'.replace(
-                                           ' ', '.')))) \
-    .click()
+       .click()
+time.sleep(10)
+processing_time_element = WebDriverWait(driver, 10).until(
+      EC.visibility_of_element_located((By.XPATH, "/html/body/main/div[3]/div[4]/div[3]/div/div[2]/p/span"))
+)
 
-
-
-WebDriverWait(driver, 5) \
-    .until(EC.element_to_be_clickable((By.NAME,
-                                       'wb-fieldflowwb-auto-1wb-auto-48'.replace(
-                                           ' ', '.')))) \
-    .click()
-# WebDriverWait(driver, 5) \
-#     .until(EC.element_to_be_clickable((By.NAME,
-#                                        'wb-fieldflowwb-auto-1wb-auto-23'))) \
-#     .send_keys('Temporary residence (visiting, studying, working)')
-
-# WebDriverWait(driver, 5) \
-#     .until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-#                                        'i.poi.poi-city'))) \
-#     .click()
-#
-# WebDriverWait(driver, 5) \
-#     .until(EC.element_to_be_clickable((By.XPATH,
-#                                        '/html/body/div[5]/main/div[4]/div/section[5]/section/div/article/section[1]/ul/li[2]/h2/a'))) \
-#     .click()
-#
-# WebDriverWait(driver, 5) \
-#     .until(EC.element_to_be_clickable((By.XPATH,
-#                                        '/html/body/div[5]/main/div[4]/div/section[5]/section/div[1]/ul')))
-#
-# texto_columnas = driver.find_element_by_xpath('/html/body/div[5]/main/div[4]/div/section[5]/section/div[1]/ul')
-# texto_columnas = texto_columnas.text
-# tiempo_hoy = texto_columnas.split('Mañana')[0].split('\n')[1:-1]
-#
-# horas = list()
-# temp = list()
-# v_viento = list()
-#
-# for i in range(0, len(tiempo_hoy), 4):
-#     horas.append(tiempo_hoy[i])
-#     temp.append(tiempo_hoy[i + 1])
-#     v_viento.append(tiempo_hoy[i + 2])
-#
-# df = pd.DataFrame({'Horas': horas, 'Temperatura': temp, 'Viento_kmxh': v_viento})
-# print(df)
-# df.to_csv('tiempo_hoy_Barcelona.csv', index=False)
-# driver.close()
+print("span_element", processing_time_element.text)
+driver.save_screenshot("screenshotfinal.png")
+driver.quit()
